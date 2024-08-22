63ebaa05b13aeeff937e5ac96a5196019674b388
AJ <aj126501@student.wpsok.org>
2024-08-22 13:42:02 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,11 @@
+#Name: AJ
+#Class: 6th hour
+#Assignment HW1
+
+print("Hola Mundo!")
+name = input("What's your name?")
+print("Hello",name)
+
+x = int(input("Give me a numba"))
+y = int(input("Give me anotha numba"))
+print(x+y)
